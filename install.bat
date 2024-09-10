30e292a6fa214324c409f088968d780d3b223708
Andrea Piccinelli <andrea.piccinelli@imagovision.it>
2024-09-10 15:32:26 +0200

    Fix ricerca certificati se la directory del bat (o una sua parent) conteneva spazi

--- install.bat
+++ install.bat
@@ -29,9 +29,9 @@ Pause
 goto :eof
 
 :InstallCertificates
-for %%f in (%~dp0%1\*.der) do (
+for %%f in ("%~dp0%1\*.der") do (
     <nul set /p=Installing: %%~nxf... 
-    certmgr.exe -add %%f -s -r localMachine %2 >temp_output.txt 2>&1
+    certmgr.exe -add "%%f" -s -r localMachine %2 >temp_output.txt 2>&1
     if errorlevel 0 (
         echo OK
         del temp_output.txt
